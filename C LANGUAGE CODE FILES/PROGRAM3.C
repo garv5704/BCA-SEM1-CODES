04a152f221d6fabaf7e7a0b025664284d0ef24c6
stdudent apnacollege <student@apnacollege.in>
2023-09-17 15:34:30 +0530

    PROGRAM3

---
+++ C LANGUAGE CODE FILES/PROGRAM3.C
@@ -0,0 +1,21 @@
+# include<stdio.h>
+int main(){
+    int length; int breadth; int radius; int aor; int perimeter; int aoc; int circumference;
+
+    printf("Enter the length and breadth for rectangle and radius for circle :\n");
+    scanf("%d%d%d", &length, &breadth, &radius);
+
+    aoc = (3.14*radius*radius);
+    circumference = 2*3.14*radius;
+    aor = length*breadth;
+    perimeter = 2*(length+breadth);
+
+    printf(" this is the area of circle :%d\n", aoc);
+    printf(" this is the circumference of circle :%d\n", circumference);
+
+    printf(" this is the area of rectangle :%d\n", aor);
+    printf(" this is the perimeter of rectangle :%d\n", perimeter);
+
+
+
+}
