bde3cbcdd2953e0b94a4154ddb083c8532ad25da
stdudent apnacollege <student@apnacollege.in>
2023-09-20 19:51:28 +0530

    PROGRAM1.c

--- C LANGUAGE CODE FILES/PROGRAM1.c
+++ C LANGUAGE CODE FILES/PROGRAM1.c
@@ -1,18 +1,18 @@
 # include <stdio.h>
 int main(){
-    int dist, m, feet, inch, cm;
+    float dist, m, feet, inch, cm;
 
     printf("Enter any distance in km\n");
 
-    scanf("%d",&dist);
+    scanf("%f",&dist);
 
     m = dist*1000;
-    feet = dist*3280;
-    inch = dist*39370;
+    feet = dist*3280.84;
+    inch = dist*39370.1;
     cm = dist*100000;
 
-    printf("this is your distance in metres :%d\n", m);
-    printf("this is your distance in feets :%d\n",feet);
-    printf("this is your distance in inches :%d\n",inch);
-    printf("this is your distance in cm :%d\n", cm);
+    printf("this is your distance in metres :%f\n", m);
+    printf("this is your distance in feets :%f\n",feet);
+    printf("this is your distance in inches :%f\n",inch);
+    printf("this is your distance in cm :%f\n", cm);
 }
