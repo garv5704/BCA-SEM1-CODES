f44c49e7468a1b45300ce966582490e5dabece85
stdudent apnacollege <student@apnacollege.in>
2023-10-07 21:45:26 +0530

    PROGRAM(WORO).C

---
+++ C LANGUAGE CODE FILES/PROGRAM5(WORO).C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+
+    printf("Enter the a and b values :\n");
+    scanf("%d%d",&a,&b);
+
+    if (a>b)
+    {
+        printf("a is greater value.");
+    }
+
+    else if (a==b)
+    {
+        printf("a is equals to b");
+    }
+
+    else 
+    {
+        printf("b is greater value.");
+    }
+    
+    return 0;
+
+    
+}
