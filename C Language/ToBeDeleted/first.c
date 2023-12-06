3b6b306c0cbdf7e09d2ae2d857a01c2b0566cf8b
stdudent apnacollege <student@apnacollege.in>
2023-12-06 18:45:59 +0530

    first.c

---
+++ C Language/ToBeDeleted/first.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+    char operator;
+    int a,b,sum;
+
+    printf("Enter an operator : ");
+    scanf("%c",&operator);
+
+    printf("Enter the two values : ");
+    scanf("%d%d",&a,&b);
+
+    switch (operator)
+    {
+    case '+':
+        sum = a+b;
+        break;
+    case '-':
+        sum = a-b;
+        break;
+    case '*':
+        sum = a*b;
+        break;
+    case '/':
+        sum = a/b;
+        break;
+    
+    default:
+    printf("Not an operator");
+        break;
+    }
+
+    printf("%d %c %d = %d", a, operator, b, sum);
+    return 0;
+}
+
+//CUSTOMIZED PROGRAM TO PERFORM MATHEMATICAL CALCULATIONS ON GIVEN OPERATOR AND OPERAND BY USER.
+//CUSTOMIZED PROGRAM TO PERFORM MATHEMATICAL CALCULATIONS ON GIVEN OPERATOR AND OPERAND BY USER.
